0286c2a3cb2ac2152e9355ebb463a406f98fdafc
DiogoValenteC <di.valentec@gmail.com>
2024-09-11 12:39:01 +0000

    add: oi

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 class Tarefa {
 
     private String titulo;
@@ -5,7 +8,7 @@ class Tarefa {
     private String prazo;
     private int prioridade;
 
-    //construtor
+    // Construtor 1
     public Tarefa(String titulo, String descricao, String prazo, int prioridade) {
         this.titulo = titulo;
         this.descricao = descricao;
@@ -13,19 +16,78 @@ class Tarefa {
         this.prioridade = prioridade;
     }
 
+    // Construtor 2
+    public Tarefa(String titulo, String prazo) {
+        this.titulo = titulo;
+        this.prazo = prazo;
+        this.descricao = "";
+        this.prioridade = 0;
+    }
+
+    // Métodos getters
     public String getTitulo() {
         return this.titulo;
     }
 
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public String getPrazo() {
+        return this.prazo;
+    }
+
+    public int getPrioridade() {
+        return this.prioridade;
+    }
+
+    // Métodos setters
     public void setTitulo(String titulo) {
         this.titulo = titulo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPrazo(String prazo) {
+        this.prazo = prazo;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    // Método para exibir detalhes da tarefa
+    public void exibirDetalhes() {
+        System.out.println("Título: " + this.getTitulo());
+        System.out.println("Descrição: " + this.getDescricao());
+        System.out.println("Prazo: " + this.getPrazo());
+        System.out.println("Prioridade: " + this.getPrioridade());
+    }
+
+    //Método para calcular dias restantes
+    public long calcularDiasRestantes () {
+        LocalDate dataPrazo = LocalDate.parse
+        (this.prazo);
+        LocalDate hoje = LocalDate.now() ;
+
+        return ChronoUnit.DAYS.between(hoje, dataPrazo);
+
     }
 }
 
 public class Main {
-    
+
     public static void main(String[] args) {
-        Tarefa tarefa1 = new Tarefa("estudar POO", "revisar conceitos de classes e objetos", "04,09,2024", 1);
-        System.out.println("Título: " + tarefa1.getTitulo());
-    }
+        // Criando primeira tarefa
+        Tarefa tarefa1 = new Tarefa("Estudar POO", "Revisar conceitos de classes e objetos", "24-09-05", 4);
+        tarefa1.exibirDetalhes();
+
+        // Criando segunda tarefa com o segundo construtor
+        Tarefa tarefa2 = new Tarefa("Estudar para provas", "2050-12-15");
+        tarefa2.exibirDetalhes();
+
+        System.out.println("Dias restantes para a tarefa 1: " + tarefa2.calcularDiasRestantes());   
+     }
 }
